b1cb9a68f9b6a93778a447ebda9a507754bd7271
Sheeza-Raees <57397108+Sheeza-Raees@users.noreply.github.com>
2019-12-06 20:45:37 +0500

    Add files via upload

---
+++ Datapreprocessing.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 17:14:44 2019
+
+@author: Sheeza
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('aids1.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
